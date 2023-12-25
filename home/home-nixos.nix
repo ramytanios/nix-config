73b8b6f84cfacd7994b721eb825fecb924ba8741
Ramy Tanios <ramy.tanios@gmail.com>
2023-12-25 12:10:13 +0100

    tweak colorschemes

--- home/home-nixos.nix
+++ home/home-nixos.nix
@@ -1,4 +1,7 @@
 { inputs, ... }: {
   imports = [ inputs.kauz.homeModules.default ];
   colorschemes.kauz.enable = true;
+
+  # kitty font specification
+  programs.kitty.font.size = 12.5;
 }
--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -3,7 +3,7 @@
   programs.kitty = {
     enable = true;
     font.name = "JetBrainsMono Nerd Font Mono";
-    font.size = 15.5;
+    # font.size = 15.5; # override for each machine
     extraConfig = ''
       cursor_shape  block
       shell_integration no-cursor
