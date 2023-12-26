728ca6a1ee4e63884bd2af050715391caa9b3f79
Ramy Tanios <ramy.tanios@gmail.com>
2023-12-26 12:35:47 +0100

    added fish function for vi mode prompts

--- home/fish/fish.nix
+++ home/fish/fish.nix
@@ -16,6 +16,31 @@
       set pure_color_mute yellow
     '';
 
+    functions = {
+      fish_mode_prompt = {
+        body = ''
+            switch $fish_bind_mode
+            case default
+              set_color --bold red
+              echo '[N]'
+            case insert
+              set_color --bold green
+              echo '[I]'
+            case replace_one
+              set_color --bold green
+              echo '[R]'
+            case visual
+              set_color --bold brmagenta
+              echo '[V]'
+            case '*'
+              set_color --bold red
+              echo '[?]'
+          end
+          set_color normal 
+        '';
+      };
+    };
+
   };
 
 }
