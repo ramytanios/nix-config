7cd8b2a776868b5999764d94396acc036e1b3ecf
Ramy Tanios <ramy.tanios@gmail.com>
2023-12-31 15:13:53 +0100

    nix darwin config

--- system/configuration-darwin.nix
+++ system/configuration-darwin.nix
@@ -1,2 +1,61 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Make sure the nix daemon always runs
+  services.nix-daemon.enable = true;
+
+  # Enable sudo authentication with Touch ID
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # Auto hide dock
+  system.defaults.dock.autohide = true;
+
+  nix.package = pkgs.nix; # this is the default
+
+  # `home-manager` currently has issues adding them to `~/Applications`
+  # Issue: https://github.com/nix-community/home-manager/issues/1341
+  environment.systemPackages = with pkgs;
+    [
+      # kitty
+    ];
+
+  # Fonts 
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs;
+    [
+      (nerdfonts.override {
+        fonts = [ "FiraCode" "JetBrainsMono" "DroidSansMono" ];
+      })
+    ];
+
+  # homebrew 
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+    };
+    casks = [
+      "discord"
+      "docker"
+      "firefox"
+      "kitty"
+      "mattermost"
+      "telegram"
+      "whatsapp"
+    ];
+  };
+
+  # ZSH
+  programs.zsh.enable = true;
+
+  # Allow unfree packages 
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.ramytanios = {
+    name = "ramytanios";
+    home = "/Users/ramytanios";
+  };
 }
