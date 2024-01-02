7648a0e23701cf1cd4c266e912a0594482fe56fe
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-02 13:55:12 +0100

    added latex

--- home/home-macos.nix
+++ home/home-macos.nix
@@ -1,7 +1,10 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [ inputs.kauz.homeModules.default ];
   colorschemes.kauz.enable = true;
 
   # kitty font specification
   programs.kitty.font.size = 15.5;
+
+  home.packages = with pkgs; [ texlive.combined.scheme-full ];
+
 }
