52a8132cf8263df8f7a09a7da3033b5a51949f99
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-02 19:54:03 +0100

    update kauz; tokyonight

--- home/home-macos.nix
+++ home/home-macos.nix
@@ -1,6 +1,13 @@
 { inputs, pkgs, ... }: {
   imports = [ inputs.kauz.homeModules.default ];
-  colorschemes.kauz.enable = true;
+
+  # Kauz colorscheme 
+  kauz = {
+    fish.enable = true;
+    tmux.enable = true;
+    kitty.enable = true;
+    neovim.enable = true;
+  };
 
   # kitty font specification
   programs.kitty.font.size = 15.5;
--- home/home-nixos.nix
+++ home/home-nixos.nix
@@ -1,7 +1,14 @@
 { inputs, ... }: {
   imports = [ inputs.tokyonight.homeModules.colorscheme ];
-  colorscheme.tokyonight.enable = true;
-  colorscheme.tokyonight.variant = "storm";
+
+  # Tokyonight colorscheme
+  colorscheme.tokyonight = {
+    variant = "storm";
+    fish.enable = true;
+    tmux.enable = true;
+    kitty.enable = true;
+    neovim.enable = true;
+  };
 
   # kitty font specification
   programs.kitty.font.size = 12.5;
