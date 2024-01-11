ca393d9e69576fce3debf0ccde43db7f911e8f6f
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-11 15:54:48 +0100

    fix fish shell

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -1,4 +1,22 @@
 { pkgs, ... }:
 
 let core = import ./core.nix { inherit pkgs; };
-in { programs.fish = { inherit (core) shellAliases; }; }
+in {
+  programs.fish = {
+    enable = true;
+
+    plugins = [{
+      name = "pure";
+      inherit (pkgs.fishPlugins.pure) src;
+    }];
+
+    interactiveShellInit = ''
+      fish_vi_key_bindings
+      ${pkgs.thefuck}/bin/thefuck --alias | source
+    '';
+
+    functions = { };
+
+    inherit (core) shellAliases;
+  };
+}
