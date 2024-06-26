ae15605092ddce5887aae872f494a947cee214f7
Ramy Tanios <ramy.tanios@gmail.com>
2024-06-26 22:37:17 +0200

    lazy nvim-treesitter; add incremental selection

--- home/nvim/plugins/treesitter.lua
+++ home/nvim/plugins/treesitter.lua
@@ -1,5 +1,20 @@
 require("nvim-treesitter.configs").setup({
 	highlight = { enable = true },
+
 	indent = { enable = true },
---	auto_install = true,
+
+	--load the plugin only when opening a buffer
+	--for an already existing file or for a new one
+	event = { "BufReadPre", "BufNewFile" },
+
+	-- enable incremental selection
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
 })
