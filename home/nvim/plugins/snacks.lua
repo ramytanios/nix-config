febfbbcec880169e09b439bd87390f7bfe77a08b
Ramy Tanios <ramy.tanios@gmail.com>
2024-12-26 09:25:12 +0100

    disable statuscolumn from snacks

--- home/nvim/plugins/snacks.lua
+++ home/nvim/plugins/snacks.lua
@@ -3,7 +3,7 @@ snacks.setup({
 	scroll = { enabled = true, animate = { duration = { step = 10, total = 1000 }, easing = "linear" } },
 	words = { enabled = true },
 	indent = { enabled = true },
-	statuscolumn = { enabled = true },
+	-- statuscolumn = { enabled = true },
 	bigfile = { enabled = true },
 	input = { enabled = true },
 	notifier = { enabled = true },
