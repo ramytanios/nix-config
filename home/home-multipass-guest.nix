8f197dacdc454275a96f85a9f20f7d2ccc8b1410
Ramy Tanios <ramy.tanios@gmail.com>
2025-11-16 18:05:38 +0100

    add home config for multipass guest

--- home/home-multipass-guest.nix
+++ home/home-multipass-guest.nix
@@ -1,1 +1,24 @@
-{ pkgs, lib, ... }: { }
+{ ... }:
+{
+
+  catppuccin =
+    let
+      flavor = "frappe";
+    in
+    {
+      enable = true;
+      inherit flavor;
+      tmux.enable = false;
+      zsh-syntax-highlighting.enable = true;
+      zsh-syntax-highlighting.flavor = flavor;
+    };
+
+  programs = {
+
+    kitty.font.size = 15.5;
+
+    git.diff-so-fancy.enable = true;
+
+  };
+
+}
