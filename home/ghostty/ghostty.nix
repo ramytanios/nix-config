bf65b0b5df0fe6455e01a4e58887933327411845
Ramy Tanios <ramy.tanios@gmail.com>
2025-11-18 20:34:39 +0100

    adjust ghostty opacity

--- home/ghostty/ghostty.nix
+++ home/ghostty/ghostty.nix
@@ -13,7 +13,7 @@
       shell-integration = "fish";
       mouse-hide-while-typing = true;
       window-save-state = "always";
-      background-opacity = 0.9;
+      background-opacity = 0.92;
       background-blur = true;
     };
   };
